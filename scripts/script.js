ae7c0f5357cd871e69a41e1e77c1c40ed5591eca
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-04-17 15:54:50 +0200

    Add sync checkboxes functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const controls = [
+    {
+      selector: '.clear',
+    },
+    {
+      selector: '.grid-toggler',
+    },
+    {
+      selector: 'zoom',
+    },
+    {
+      selector: '.marker-color',
+    },
+    {
+      selector: '.bg-color',
+    },
+    {
+      selector: '.marker',
+    },
+    {
+      selector: '.eraser',
+    },
+    {
+      selector: '.randomizer',
+    },
+    {
+      selector: '.color-picker',
+    },
+    {
+      selector: '.lightener',
+    },
+    {
+      selector: '.darkener',
+    },
+  ];
+
+  for (const control of controls) {
+    control.inputNodes = document.querySelectorAll(control.selector);
+  }
+  
+  for (const control of controls) {
+    const inputs = [...control.inputNodes];
+
+    if (inputs.every(node => node.nodeName === 'BUTTON')) continue;
+
+    if (inputs.every(node => node.type === 'checkbox')) {
+      syncCheckboxes(inputs);
+    } else {
+      console.log(inputs);
+    }
+    
+  }
+
   document.querySelectorAll('[type=color]').forEach(input => {
     input.addEventListener('input', event => {
       updateIconColor(event.currentTarget);
     });
   });
   
-  const markerControls = document.querySelectorAll('input[name="marker-options"]');
+  const markerControls = document.querySelectorAll('[name="marker-options"]');
 
   markerControls.forEach(input => {
     input.addEventListener('click', event => {
@@ -19,6 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
 });
+
 
 function calculateLuminance(color) {
   const { r, g, b } = color;
@@ -41,5 +95,17 @@ function updateIconColor(colorInput) {
   icon.style.color = (luminance > 0.5) 
     ? 'var(--color-button-icon)' 
     : 'var(--color-text)';
+}
+
+function syncCheckboxes(pair) {
+  const [checkbox1, checkbox2] = pair;
+
+  checkbox1.addEventListener('change', () => {
+    checkbox2.checked = checkbox1.checked;
+  });
+
+  checkbox2.addEventListener('change', () => {
+    checkbox1.checked = checkbox2.checked;
+  });
 }
 
